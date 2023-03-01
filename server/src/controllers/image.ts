a2ebb91483210ea695d2dc7812c639647a7c9f57
ahmedgamer4 <aa10@pme.suezuni.edu.eg>
2023-03-01 11:29:20 +0200

    Finished

--- server/src/controllers/image.ts
+++ server/src/controllers/image.ts
@@ -1,45 +1,7 @@
 import express from 'express'
-import https from 'https'
 import { cloudinary, upload } from '../utils/cloudnary.js'
 
 export const imageRouter = express.Router()
-
-const urlToBuffer = async (url: string): Promise<Buffer> => {
-  return new Promise((resolve, reject) => {
-    const data: Uint8Array[] = []
-
-    https.get(url, (response) => {
-      response
-        .on('data', (chunk: Uint8Array) => {
-          data.push(chunk)
-        })
-        .on('end', () => {
-          resolve(Buffer.concat(data))
-        })
-        .on('error', (error) => {
-          reject(error)
-        })
-    })
-  })
-}
-
-imageRouter.get(
-  '/uploads/:filename',
-  async (req: express.Request, res: express.Response) => {
-    try {
-      const imageBuffer: Buffer = await urlToBuffer(
-        cloudinary.url(`images/${req.params.filename}.jpg`, {
-          secure: true,
-        })
-      )
-      res.writeHead(200, { 'Content-Type': 'image/jpg' })
-      res.write(imageBuffer)
-      res.end()
-    } catch (err) {
-      res.status(500).send(err)
-    }
-  }
-)
 
 imageRouter.post('/uploads', upload.single('image'), async (req, res) => {
   try {
